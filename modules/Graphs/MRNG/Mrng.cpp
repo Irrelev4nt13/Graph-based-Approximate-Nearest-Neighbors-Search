ded710b5de338cab6bdb9a553c9ffde8d3d40107
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-11-26 17:23:44 +0200

    code cleanup

--- modules/Graphs/MRNG/Mrng.cpp
+++ modules/Graphs/MRNG/Mrng.cpp
@@ -8,99 +8,20 @@
 #include "Mrng.hpp"
 #include "GraphTypes.hpp"
 #include "Utils.hpp"
-#include "Lsh.hpp"
 #include "BruteForce.hpp"
 
-Mrng::Mrng(const std::vector<ImagePtr> &images, int l) : distHelper(ImageDistance::getInstance()), candidates(l)
+Mrng::Mrng(const std::vector<ImagePtr> &images, int l) : distHelper(ImageDistance::getInstance()), navNode(nullptr), candidates(l)
 {
-    // brute force. Uncomment to use
-    // useBruteForce(images);
-
-    // lsh method. Uncomment to use
-    // startClock();
-    // Lsh *lsh = new Lsh(images, 4, 5, (int)images.size() - 1, 2240, (int)images.size() / 8);
-    // auto lshDuration = stopClock();
-    // std::cout << "lsh construction finished in: " << lshDuration.count() * 1e-9 << std::endl;
-
     startClock();
-
-    useBruteForce(images);
-
-    auto mrngDuration = stopClock();
-    std::cout << "mrng construction finished in: " << mrngDuration.count() * 1e-9 << std::endl;
-
-    // delete lsh;
-}
-
-// void Mrng::useBruteForce(const std::vector<ImagePtr> &images)
-// {
-//     for (std::size_t i = 0; i < images.size(); i++)
-//     {
-//         startClock();
-//         std::vector<ImagePtr> Rp = images;
-
-//         Rp.erase(Rp.begin() + i);
-
-//         std::sort(Rp.begin(), Rp.end(), CompDistanceToRefImage(images[i], distHelper));
-
-//         auto duration = stopClock();
-
-//         std::cout << "duration: " << duration.count() * 1e-9 << std::endl;
-
-//         std::vector<ImagePtr> Lp;
-
-//         Lp.push_back(Rp[0]);
-
-//         for (const auto &r : Rp)
-//         {
-//             if (std::find(Lp.begin(), Lp.end(), r) != Lp.end())
-//             {
-//                 continue; // r found in Lp
-//             }
-
-//             bool condition = true;
-//             for (const auto &t : Lp)
-//             {
-//                 double prDistance = distHelper->calculate(images[i], r);
-//                 double ptDistance = distHelper->calculate(images[i], t);
-//                 double rtDistance = distHelper->calculate(r, t);
-
-//                 if (prDistance > ptDistance && prDistance > rtDistance)
-//                 {
-//                     condition = false;
-//                     break; // pr is the longest edge, so r is not added to Lp.
-//                 }
-//             }
-
-//             if (condition)
-//             {
-//                 Lp.push_back(r);
-//             }
-//         }
-
-//         graph.push_back(Lp);
-//     }
-// }
-
-void Mrng::useBruteForce(const std::vector<ImagePtr> &images)
-{
-    this->navNode = nullptr;
 
     for (std::size_t i = 0; i < images.size(); i++)
     {
-        // Get Rp sorted by Lsh distance approximation algorithm
-        // Lsh *tempLsh = new Lsh(images, 4, 5, images.size(), 2240, images.size() / 8);
-
         std::vector<Neighbor> Rp = BruteForce(images, images[i], images.size() - 1);
-
-        // delete tempLsh;
 
         if (!this->navNode)
         {
-            this->navNode = new Neighbor(Rp[images.size() / 2]);
+            this->navNode = Rp[images.size() / 2].image;
         }
-
-        // startClock();
 
         // Initialize Lp with points that have minimum distance to p (images[i])
         std::vector<ImagePtr> Lp;
@@ -141,25 +62,20 @@ void Mrng::useBruteForce(const std::vector<ImagePtr> &images)
             }
         }
 
-        // auto duration = stopClock();
-
-        // std::cout << "duration: " << duration.count() * 1e-9 << std::endl;
-
         graph.push_back(Lp);
     }
+
+    auto mrngDuration = stopClock();
+    std::cout << "mrng construction finished in: " << mrngDuration.count() * 1e-9 << std::endl;
 }
 
-Mrng::~Mrng()
-{
-    std::cout << "destructor" << std::endl;
-    delete navNode;
-}
+Mrng::~Mrng() {}
 
 std::vector<Neighbor> Mrng::Approximate_kNN(ImagePtr query, int k)
 {
     std::vector<ImagePtr> R;
 
-    ImagePtr p = navNode->image;
+    ImagePtr p = navNode;
 
     R.push_back(p);
 
--- modules/Graphs/MRNG/Mrng.hpp
+++ modules/Graphs/MRNG/Mrng.hpp
@@ -11,25 +11,11 @@ class Mrng : public GraphAlgorithm
 private:
     ImageDistance *distHelper;
     std::vector<std::vector<ImagePtr>> graph;
-    Neighbor *navNode;
+    ImagePtr navNode;
     int candidates;
-
-    void useBruteForce(const std::vector<ImagePtr> &images);
-    void useLsh(const std::vector<ImagePtr> &images, Lsh *lsh);
 
 public:
     Mrng(const std::vector<ImagePtr> &images, int l = 20);
     ~Mrng();
     std::vector<Neighbor> Approximate_kNN(ImagePtr query, int k);
 };
-
-// class KNNResult
-// {
-// public:
-//     ImagePtr image;
-//     double distance;
-//     bool operator>(const KNNResult &other) const
-//     {
-//         return distance > other.distance;
-//     }
-// };
